b5b818428039a87fad3d09e7f054644c159d7804
EzeVedder <pane.ezequiel@gmail.com>
2018-05-03 11:05:35 -0300

    CLASE_11 PRACTICA PARCIAL

--- LAB-PROG I/Clases/Clase_11/lib.c
+++ LAB-PROG I/Clases/Clase_11/lib.c
@@ -1,55 +1,171 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib.h"
+#define TAMANIO 10
+#define OCUPADO 0
+#define LIBRE 1
 
 
 void menu(void)
 {
-    int opcion;
-    int continuar = 0;
+    printf("\n\t MENU DE OPCIONES \n");
+    printf("\n1.ALTA DE USUARIO\n2.MODIFICAR DATOS DE USUARIO\n3.BAJA DE USUARIO\n4.PUBLICAR PRODUCTO\n5.MODIFICAR PUBLICACION\n6.CANCELAR PUBLICACION\n7.COMPRAR PRODUCTO\n8.LISTAR PUBLICACIONES DE USUARIO\n9.LISTAR PUBLICACIONES\n10.LISTAR USUARIOS\n11.SALIR\n");
+}
 
-    do
+int eGen_init( eGenerica listado[],int limite)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
     {
-        printf("\t MENU DE OPCIONES\n");
-        printf("\n1.ALTA DE USUARIO\n2.MODIFICAR DATOS DE USUARIO\n3.BAJA DE USUARIO\n4.PUBLICAR PRODUCTO\n5.MODIFICAR PUBLICACION\n6.CANCELAR PUBLICACION\n7.COMPRAR PRODUCTO\n8.LISTAR PUBLICACIONES DE USUARIO\n9.LISTAR PUBLICACIONES\n10.LISTAR USUARIOS\n11.SALIR\n");
-        printf("\nElija una opcion: \n");
-        scanf("%d",&opcion);
-
-        switch(opcion)
+        retorno = 0;
+        for(i=0; i<limite; i++)
         {
-            case 1:
-                break;
-
-            case 2:
-                break;
-
-            case 3:
-                break;
-
-            case 4:
-                break;
-
-            case 5:
-                break;
-
-            case 6:
-                break;
-
-            case 7:
-                break;
-
-            case 8:
-                break;
-
-            case 9:
-                break;
-
-            case 10:
-                break;
-
-            default:
-                break;
+            listado[i].estado= LIBRE;
+            listado[i].idGenerica= 0;
         }
+    }
+    return retorno;
+}
 
-    }while(continuar != 11);
+int eGen_buscarLugarLibre(eGenerica listado[],int limite)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2;
+        for(i=0;i<limite;i++)
+        {
+            if(listado[i].estado == LIBRE)
+            {
+                retorno = i;
+                break;
+            }
+        }
+    }
+    return retorno;
+}
+
+int eGen_siguienteId(eGenerica listado[],int limite)
+{
+    int retorno = 0;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        for(i=0; i<limite; i++)
+        {
+            if(listado[i].estado == OCUPADO)
+            {
+                    if(listado[i].idGenerica>retorno)
+                    {
+                         retorno=listado[i].idGenerica;
+                    }
+
+            }
+        }
+    }
+
+    return retorno+1;
+}
+
+
+int eGen_buscarPorId(eGenerica listado[] ,int limite, int id)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2;
+        for(i=0;i<limite;i++)
+        {
+            if(listado[i].estado == OCUPADO && listado[i].idGenerica == id)
+            {
+                retorno = i;
+                break;
+            }
+        }
+    }
+    return retorno;
+}
+
+
+
+int eGen_mostrarUno(eGenerica parametro)
+{
+     printf("\n %s - %d - %d",parametro.nombre,parametro.idGenerica,parametro.estado);
+
+}
+
+int eGen_mostrarListado(eGenerica listado[],int limite)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = 0;
+        for(i=0; i<limite; i++)
+        {
+            if(listado[i].estado==OCUPADO)
+            {
+
+                eGen_mostrarUno(listado[i]);
+            }
+        }
+    }
+    return retorno;
+}
+
+
+int eGen_mostrarListadoConBorrados(eGenerica listado[],int limite)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = 0;
+        for(i=0; i<limite; i++)
+        {
+            if(listado[i].estado==LIBRE)
+            {
+                printf("\n[LIBRE]");
+            }
+            eGen_mostrarUno(listado[i]);
+        }
+    }
+    return retorno;
+}
+
+
+
+
+int eGen_alta(eGenerica  listado[],int limite)
+{
+    int retorno = -1;
+    char nombre[50];
+    int id;
+    int indice;
+
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2;
+        indice = eGen_buscarLugarLibre(listado,limite);
+        if(indice >= 0)
+        {
+            retorno = -3;
+            id = eGen_siguienteId(listado,limite);
+
+            //if(!getValidString("Nombre?","Error","Overflow", nombre,50,2))
+            //{
+                retorno = 0;
+                strcpy(listado[indice].nombre,"juan ");
+                listado[indice].idGenerica = id;
+                listado[indice].estado = OCUPADO;
+            //}
+        }
+    }
+    return retorno;
 }
 
--- LAB-PROG I/Clases/Clase_11/main.c
+++ LAB-PROG I/Clases/Clase_11/main.c
@@ -2,16 +2,66 @@
 #include <stdlib.h>
 #include "lib.h"
 #include <conio.h>
-
-
+#define TAMANIO 10
 
 
 int main()
 {
+    int opcion;
+    int i;
 
+    do
+    {
         menu();
+        printf("\nElija una opcion: ");
+        scanf("%d",&opcion);
 
-    getch();
+        switch(opcion)
+        {
+            case 1:
+                for(i=0;i<TAMANIO;i++)
+                {
+
+                }
+
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
+
+            case 9:
+                break;
+
+            case 10:
+                break;
+
+            default:
+                break;
+        }
+
+
+    }while(opcion != 11);
+
+
+    printf("\nHASTA LUEGO...\n");
     return 0;
 }
 
