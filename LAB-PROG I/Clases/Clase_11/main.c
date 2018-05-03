a952c2850b15ee1d892eda134d34a7db83ae9794
EzeVedder <pane.ezequiel@gmail.com>
2018-05-03 11:47:33 -0300

    ABM ESTRUCTURAS --- PRACTICA PARCIAL

--- LAB-PROG I/Clases/Clase_11/main.c
+++ LAB-PROG I/Clases/Clase_11/main.c
@@ -15,52 +15,53 @@ int main()
         menu();
         printf("\nElija una opcion: ");
         scanf("%d",&opcion);
-
-        switch(opcion)
+        while(opcion <= 0 || opcion >= 12)
         {
-            case 1:
-                for(i=0;i<TAMANIO;i++)
-                {
-                    printf("Ingrese su nombre: ");
-                    fflush(stdin);
-                    printf("Ingrese su Password: ");
-                    gets(eGenerica.nombre[i]);
-
-                }
-
-                break;
-
-            case 2:
-                break;
-
-            case 3:
-                break;
-
-            case 4:
-                break;
-
-            case 5:
-                break;
-
-            case 6:
-                break;
-
-            case 7:
-                break;
-
-            case 8:
-                break;
-
-            case 9:
-                break;
-
-            case 10:
-                break;
-
-            default:
-                break;
+            printf("\nERROR. %d NO ES UNA OPCION VALIDA!!!\n",opcion);
+            printf("\nElija una opcion: ");
+            scanf("%d",&opcion);
         }
+            switch(opcion)
+            {
+                case 1:
+                    for(i=0;i<TAMANIO;i++)
+                    {
 
+
+                    }
+
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+
+                default:
+                    break;
+            }
 
     }while(opcion != 11);
 
